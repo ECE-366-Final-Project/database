404d40dc0b1351c06329221e2caf2e1ef3e90fd7
flof <evanrose99@gmail.com>
2024-02-25 16:53:21 -0500

    database script and user test implemented

---
+++ init.sql
@@ -0,0 +1,79 @@
+-- ENUM : exampleEnum_ENUM
+-- Table: example_table
+-- Column : example_column
+
+CREATE TABLE "user" (
+  "username" varchar(16) UNIQUE NOT NULL,
+  "user_id" SERIAL PRIMARY KEY,
+  "balance" numeric(19,2) NOT NULL DEFAULT 0,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TYPE transactionType_ENUM as enum('deposit', 'withdrawal');
+
+CREATE TABLE "transaction_history" (
+  "transaction_id" SERIAL PRIMARY KEY,
+  "user_id" integer NOT NULL,
+  "trasaction_type" transactionType_ENUM NOT NULL,
+  "amount" numeric(19,2) NOT NULL
+);
+
+CREATE TYPE gameType_ENUM AS ENUM('blackjack', 'slots');
+
+CREATE TABLE "games" (
+  "game_id" SERIAL PRIMARY KEY,
+  "game_type" gameType_ENUM NOT NULL,
+  "user_id" integer NOT NULL,
+  "bet" numeric(19,2) NOT NULL,
+  "active" bool NOT NULL,
+  "winnings" numeric(19,2) DEFAULT null
+);
+
+CREATE TABLE "slots_symbols" (
+  "symbol_id" SERIAL PRIMARY KEY,
+  "symbol_name" varchar(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE "slots_payouts" (
+  "payout_id" SERIAL PRIMARY KEY,
+  "payout" numeric(10,5) NOT NULL DEFAULT 1,
+  "roll_1" integer DEFAULT null,
+  "roll_2" integer DEFAULT null,
+  "roll_3" integer DEFAULT null
+);
+
+CREATE TABLE "slots" (
+  "game_id" integer PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "bet" numeric(19,2) NOT NULL,
+  "payout_id" integer,
+  "winnings" numeric(19,2) DEFAULT null
+);
+
+CREATE TABLE "blackjack" (
+  "game_id" integer UNIQUE PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "bet" numeric(19,2) NOT NULL,
+  "player_hand" varchar(44) DEFAULT null,
+  "dealer_hand" varchar(22) DEFAULT null
+);
+
+ALTER TABLE "transaction_history" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+
+ALTER TABLE "slots_payouts" ADD FOREIGN KEY ("roll_1") REFERENCES "slots_symbols" ("symbol_id");
+
+ALTER TABLE "slots_payouts" ADD FOREIGN KEY ("roll_2") REFERENCES "slots_symbols" ("symbol_id");
+
+ALTER TABLE "slots_payouts" ADD FOREIGN KEY ("roll_3") REFERENCES "slots_symbols" ("symbol_id");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("game_id");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+
+ALTER TABLE "slots" ADD FOREIGN KEY ("payout_id") REFERENCES "slots_payouts" ("payout_id");
+
+ALTER TABLE "blackjack" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("game_id");
+
+ALTER TABLE "blackjack" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
---
+++ userTest.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO public.user(username, balance, created_at)
+VALUES ('flof', 1000069, NOW());
+
+INSERT INTO public.user(username, balance, created_at)
+VALUES ('hwbush', 4, NOW());
